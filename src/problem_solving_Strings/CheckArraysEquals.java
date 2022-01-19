c3e73fdf9c7ad6982cb6380225c8eec60c6ca7df
arfoysal <arfoysal.me@gmail.com>
2022-01-19 14:26:56 +0600

    Add checking equality of two Arrays problem solve

--- src/problem_solving_Strings/CheckArraysEquals.java
+++ src/problem_solving_Strings/CheckArraysEquals.java
@@ -1,59 +1,57 @@
 package problem_solving_Strings;
 
 import java.util.Arrays;
-import java.util.Iterator;
 
 public class CheckArraysEquals {
 	/*
 	 * Write a function that takes two Arrays and check the Equality of two Arrays
 	 */
-	
+
 	public static void main(String[] args) {
-		
-		int a[]= {1,2,3,4,5,6};
-		int b[]= {1,2,3,4,5,6};
-		
+
+		int a[] = { 1, 2, 3, 4, 5, 6 };
+		int b[] = { 1, 2, 3, 4, 5, 6 };
+
 		// Using Arrays class
 		arrysEqualsCheck(a, b);
-		
+
 		// Using condition and loop
 		arrysEqualsCheckUsingLoop(a, b);
-		
 
 	}
+
 	// Using Arrays class
 	public static void arrysEqualsCheck(int[] a, int[] b) {
 		if (Arrays.equals(a, b)) {
 			System.out.println("Arrays are Equals");
-			
-		}else {
+
+		} else {
 			System.out.println("Arrays are not Equals");
-			
+
 		}
-		
+
 	}
-	
+
+	// Using condition and loop
 	public static void arrysEqualsCheckUsingLoop(int[] a, int[] b) {
 		boolean status = true;
 		if (a.length == b.length) {
-			for(int i = 0; i < a.length; i++) {
+			for (int i = 0; i < a.length; i++) {
 				if (a[i] != b[i]) {
 					status = false;
 					break;
 				}
 			}
-		}else {
+		} else {
 			status = false;
 		}
 		if (status) {
 			System.out.println("Arrays are Equals");
-			
-		}else {
+
+		} else {
 			System.out.println("Arrays are not Equals");
-			
+
 		}
 	}
-	
-	
 
 }
