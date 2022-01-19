9c3ce7c54220b7e0b5e673e3d7a2941418900ab7
arfoysal <arfoysal.me@gmail.com>
2022-01-19 22:42:49 +0600

    Add Find duplicate elements in Arry problem solve

---
+++ src/problem_solving_Strings/FindDuplicateIElementsInArray.java
@@ -0,0 +1,56 @@
+package problem_solving_Strings;
+
+import java.util.HashSet;
+
+public class FindDuplicateIElementsInArray {
+	/*
+	 * Write a function that takes a Array as input and Find is there a duplicate
+	 * elements in Array or not?
+	 */
+
+	public static void main(String[] args) {
+		String langs[] = { "java", "python", "c", "C++", "java" };
+
+		// Using loop and Condition
+		checkDuplicatByLoop(langs);
+
+		// Using HashSet -- a set can't store duplicate value
+		checkDuplicatByHashSet(langs);
+	}
+
+	// Using HashSet -- a set can't store duplicate value
+	private static void checkDuplicatByHashSet(String[] langs) {
+		boolean flag = true;
+		HashSet<String> hashSet = new HashSet<String>();
+		for (String item : langs) {
+			if (!hashSet.add(item)) {
+				flag = false;
+			}
+		}
+		if (flag) {
+			System.out.println("Duplicate Element not Found");
+		} else {
+			System.out.println("Duplicate Element Found");
+		}
+
+	}
+
+	// Using loop and Condition
+	private static void checkDuplicatByLoop(String[] langs) {
+		boolean flag = true;
+
+		for (int i = 0; i < langs.length; i++) {
+			for (int j = i + 1; j < langs.length; j++) {
+				if (langs[i] == langs[j]) {
+					flag = false;
+				}
+			}
+		}
+		if (flag) {
+			System.out.println("Duplicate Element not Found");
+		} else {
+			System.out.println("Duplicate Element Found");
+		}
+
+	}
+}
